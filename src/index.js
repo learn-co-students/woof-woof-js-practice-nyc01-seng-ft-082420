38b3bca4a0244eb4713e38c2840b46e43b11ad99
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-12 18:03:40 -0400

    dog info loading correctly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchDogs()
+    selectionHandler()
 })
 
 function fetchDogs() {
@@ -10,7 +11,29 @@ function fetchDogs() {
         for (pup of pups) {
             const dog = document.createElement("span")
             dog.textContent = pup.name
+            dog.dataset.id = pup.id
+            dog.dataset.pic = pup.image
+            dog.dataset.isGoodDog = pup.isGoodDog
+
             dogBar.append(dog)
         }
+    })
+}
+
+function selectionHandler() {
+    const dogInfo = document.querySelector("#dog-info")
+    const dogBar = document.querySelector("#dog-bar")
+    dogBar.addEventListener("click", e => {
+        dogInfo.innerHTML = `
+        <img src=${e.target.dataset.pic}>
+        <h2>${e.target.textContent}</h2>
+        `
+        const btn = document.createElement("button")
+        if (e.target.dataset.isGoodDog) {
+            btn.textContent = "Good Dog!"
+        } else {
+            btn.textContent = "Bad Dog!"
+        }
+        dogInfo.append(btn)
     })
 }
