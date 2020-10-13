a8d36682c24422d9cecd1f6d5843bca545a41c2a
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-12 22:38:44 -0400

    deliverables 2 and 3 redone

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchDogs()
     selectionHandler()
-    buttonHandler()
 })
 
 function fetchDogs() {
@@ -9,16 +8,38 @@ function fetchDogs() {
     .then( res => res.json())
     .then( pups => {
         const dogBar = document.querySelector("#dog-bar")
+        const dogInfo = document.querySelector("#dog-info")
         for (pup of pups) {
-            const dog = document.createElement("span")
-            dog.classList.add("dog")
-            dog.textContent = pup.name
-            dog.dataset.id = pup.id
-            dog.dataset.pic = pup.image
-            dog.dataset.isGoodDog = pup.isGoodDog
-            dogBar.append(dog)
+            renderPup(pup)
         }
     })
+}
+
+function renderPup() {
+    const dogBar = document.querySelector("#dog-bar")
+    const dogInfo = document.querySelector("#dog-info")
+    const dog = document.createElement("span")
+    dog.dataset.id = pup.id
+    dog.textContent = pup.name
+    dog.classList.add("dog")
+    dogBar.append(dog)
+
+    const dogDiv = document.createElement("div")
+    dogDiv.classList.add("dog-div")
+    dogDiv.innerHTML = `
+    <img src=${pup.image}>
+    <h2>${pup.name}</h2>
+    `
+    dogDiv.dataset.id = pup.id
+    const btn = document.createElement("button")
+    if (pup.isGoodDog) {
+        btn.textContent = "Good Dog"
+    } else {
+        btn.textContent = "Bad Dog"
+    }
+    dogDiv.append(btn)
+    dogDiv.hidden = true
+    dogInfo.append(dogDiv)
 }
 
 function selectionHandler() {
@@ -26,66 +47,19 @@ function selectionHandler() {
     const dogBar = document.querySelector("#dog-bar")
     dogBar.addEventListener("click", e => {
         if (e.target.matches(".dog")) {
-            dogInfo.innerHTML = `
-            <img src=${e.target.dataset.pic}>
-            <h2>${e.target.textContent}</h2>
-            `
-            const btn = document.createElement("button")
-            btn.dataset.id = e.target.dataset.id
-            btn.dataset.isGoodDog = e.target.dataset.isGoodDog
-            if (e.target.dataset.isGoodDog === "true") {
-                btn.textContent = "Good Dog!"
-            } else {
-                btn.textContent = "Bad Dog!"
-            }
-            dogInfo.append(btn)
+            const allDogs = document.querySelectorAll(".dog-div")
             
+            for (dog of allDogs) {
+                console.log(dog.id)
+                console.log(e.target.dataset.id)
+                if (dog.dataset.id === e.target.dataset.id) {
+                    dog.hidden = false
+                } else {
+                    dog.hidden = true
+                }
+            }
         }
     })
 }
 
-function buttonHandler() {
-    const btn = document.querySelector("#dog-summary-container")
-    btn.addEventListener("click", e => {
-        console.log(e.target.dataset.isGoodDog)
-        if (e.target.matches("button")) {
-        let patch;
-        console.log(e.target.dataset.id)
-        if (e.target.dataset.isGoodDog === "true") {
-            patch = {
-                isGoodDog: "false"
-            }
-        } else {
-            patch = {
-                isGoodDog: "true"
-            }
-        }
-        console.log(patch)
-        const configObj = {
-            method: "PATCH",
-            headers: {
-                "content-type": "application/json",
-                accept: "application/json"
-            },
-            body: JSON.stringify(patch)
-        }
-        fetch(`http://localhost:3000/pups/${e.target.dataset.id}`, configObj)
-        .then( res => res.json() )
-        .then( obj => {
-            console.log(obj)
-            if (obj.isGoodDog === "true") {
-                e.target.dataset = "true"
-                textContent = "Good Dog!"
-            } else {
-                e.target.dataset = "false"
-                e.target.textContent = "Bad Dog!"
-            }
-            console.log(e.target.dataset.isGoodDog)
-        })
-    }
-        
-    })
     
-    
-    
-}
