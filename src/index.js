f25ccfd40d530a84e2b3c87a66678dbcc26d44f4
jessicasalbert <jessicarsalbert@gmail.com>
2020-10-12 23:08:25 -0400

    button toggle working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     fetchDogs()
     selectionHandler()
+    toggleButton()
 })
 
 function fetchDogs() {
@@ -32,7 +33,7 @@ function renderPup() {
     `
     dogDiv.dataset.id = pup.id
     const btn = document.createElement("button")
-    if (pup.isGoodDog) {
+    if (pup.isGoodDog === "true") {
         btn.textContent = "Good Dog"
     } else {
         btn.textContent = "Bad Dog"
@@ -48,10 +49,7 @@ function selectionHandler() {
     dogBar.addEventListener("click", e => {
         if (e.target.matches(".dog")) {
             const allDogs = document.querySelectorAll(".dog-div")
-            
             for (dog of allDogs) {
-                console.log(dog.id)
-                console.log(e.target.dataset.id)
                 if (dog.dataset.id === e.target.dataset.id) {
                     dog.hidden = false
                 } else {
@@ -63,3 +61,39 @@ function selectionHandler() {
 }
 
     
+function toggleButton() {
+    const dogInfo = document.querySelector("#dog-info")
+    dogInfo.addEventListener("click", e => {
+        if (e.target.matches("button")) {
+            const dogId = e.target.parentElement.dataset.id
+            let patch
+            if (e.target.parentElement.dataset.isGoodDog === "true") {
+                patch = "false"
+            } else {
+                patch = "true"
+            }
+            console.log(patch)
+            const configObj = {
+                method : "PATCH",
+                headers : {
+                    "content-type" : "application/json",
+                    accept : "application/json"
+                },
+                body : JSON.stringify({
+                    isGoodDog : patch
+                })
+            }
+            fetch(`http://localhost:3000/pups/${dogId}`, configObj)
+            .then(res => res.json())
+            .then(obj => {
+                if (patch === "true") {
+                    e.target.textContent = "Good Dog"
+                    e.target.parentElement.dataset.isGoodDog = "true"
+                } else {
+                    e.target.textContent = "Bad Dog"
+                    e.target.parentElement.dataset.isGoodDog = "false"
+                }
+            })
+        }
+    })
+}
